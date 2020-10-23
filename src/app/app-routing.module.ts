95d1c1af0544bdfff85e1e53907ea10bec8a22d8
Omar Avalos <omar.avalos.m@gmail.com>
2020-10-22 23:09:05 -0500

    Inicial v4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { QuienesComponent } from './components/quienes/quienes.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { PromocionesComponent } from './components/promociones/promociones.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'quienes', component: QuienesComponent},
+  { path: 'servicios', component: ServiciosComponent},
+  { path: 'productos', component: ProductosComponent},
+  { path: 'promociones', component: PromocionesComponent},
+  { path: 'contacto', component: ContactoComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
