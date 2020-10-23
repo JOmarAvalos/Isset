9b79d147e821982d2bcd469a5850fd948a4b5098
Omar Avalos <omar.avalos.m@gmail.com>
2020-10-22 21:39:52 -0500

    v3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { QuienesComponent } from './components/quienes/quienes.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { PromocionesComponent } from './components/promociones/promociones.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    QuienesComponent,
+    ServiciosComponent,
+    ProductosComponent,
+    PromocionesComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
